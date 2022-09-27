6e0ce660b0d99be8e5ebceea2cb8c17f0de80a67
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-27 15:06:07 +0200

    add front.js on webpack.mix

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,25 +1,26 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-    <div class="container">
-        <h1 class="text-center">BENVENUTO IN BOOLPRESS</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur iusto facilis ipsum, quaerat aliquid
-            molestiae maxime, numquam in quos pariatur atque, molestias exercitationem architecto corrupti esse. Beatae
-            explicabo eos consectetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores quas numquam
-            optio ipsum consequuntur! Maiores sed ullam libero? Accusantium, sint reprehenderit ratione animi suscipit saepe
-            nihil repellat fuga soluta?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet rerum nemo sint illo incidunt, modi accusamus hic
-            ut exercitationem possimus, assumenda nam perferendis quia. Beatae quam officia soluta voluptate eveniet.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur iusto facilis ipsum, quaerat aliquid
-            molestiae maxime, numquam in quos pariatur atque, molestias exercitationem architecto corrupti esse. Beatae
-            explicabo eos consectetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores quas numquam
-            optio ipsum consequuntur! Maiores sed ullam libero? Accusantium, sint reprehenderit ratione animi suscipit saepe
-            nihil repellat fuga soluta?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet rerum nemo sint illo incidunt, modi accusamus hic
-            ut exercitationem possimus, assumenda nam perferendis quia. Beatae quam officia soluta voluptate eveniet.
-        </p>
-        <div class="w-100 d-flex justify-content-center">
-            <img class="w-75" src="https://s.w.org/images/home/screen-themes.png?3" alt="img-word-press-home">
-        </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>boolpress</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/front.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="root">
+        POYOOOOOO
     </div>
-@endsection
+</body>
+
+</html>
